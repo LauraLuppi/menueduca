ad59c4636d453ce499f4848837d52d91d5ce1994
LauraLuppi <lauravtluppi15@gmail.com>
2025-03-20 14:37:51 -0300

    Cadastro feito e contexto feito

---
+++ Models/CadastroUsuario.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MenuEduca.Models
+{
+    [Table("CadastroUsuario")]
+    public class CadastroUsuario
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("NomeCompleto")]
+        [Display(Name = "Nome completo")]
+        public string NomeCompleto { get; set; } = string.Empty;
+
+        [Column("Cpf")]
+        [Display(Name = "CPF")]
+        public string Cpf { get; set; } = string.Empty;
+
+        [Column("Email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Column("Senha")]
+        [Display(Name = "Senha")]
+        public string Senha { get; set; } = string.Empty;
+
+        [Column("TipoUsuario")]
+        [Display(Name = "Tipo do usuárioo")]
+        public string TipoUsuario { get; set; } = string.Empty;
+
+    }
+}
--- Models/Contexto.cs
+++ Models/Contexto.cs
@@ -1,7 +1,18 @@
-﻿namespace MenuEduca.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MenuEduca.Models
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
-       
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<CadastroUsuario> CadastroUsuario { get; set; }
+        //public DbSet<InsercaoMedica> InsercaoMedica { get; set; }
+        //public DbSet<Cardapio> Cardapio { get; set; }
+        //public DbSet<Avaliacao> Avaliacao { get; set; }
+
     }
 }
