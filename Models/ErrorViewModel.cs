30e3ce8c13028b7e1d5f61425fcec7fae8741f55
LauraLuppi <lauravtluppi15@gmail.com>
2025-03-20 10:57:42 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MenuEduca.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
