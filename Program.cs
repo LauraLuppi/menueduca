6089285cfd0bcabeafd6f00a091872c70e711149
soriano1010 <raissasoriano51@gmail.com>
2025-03-20 14:49:52 -0300

    Program

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using MenuEduca.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<Contexto>
+   (options => options.UseSqlServer("Data Source=SB-1490648\\SQLSENAI;Initial Catalog = MenuEduca" +
+   ";Integrated Security = True;TrustServerCertificate = True"));
+
 
 var app = builder.Build();
 
